35e283defec3277a8fc132fbd348bdd101981883
WillFortes <willfortes.dev@gmail.com>
2024-06-28 14:20:11 -0300

    fix: logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Content from "@/shared/data.json";
 import Image from "next/image";
+import LogoWhite from "@/assets/logo-white.svg";
 
 const Footer = () => {
     return (
@@ -7,7 +8,7 @@ const Footer = () => {
             <div className="container m-auto">
                 <div className="grid md:grid-cols-10 sm:grid-cols-1 px-10 md:px-0">  
                     <div className="md:col-span-2 sm:col-span-1 mb-10 sm:mb-0">
-                        <Image src="/logo-white.svg" alt="ZeeCred Logo" width={182.92} height={34} />
+                        <Image src={LogoWhite} alt="ZeeCred Logo" width={182.92} height={34} />
                     </div>
                     <div className="md:col-span-8 sm:col-span-1">
                         <p className="text-sm font-light leading-5" dangerouslySetInnerHTML={{__html: Content.footer.description}} />
